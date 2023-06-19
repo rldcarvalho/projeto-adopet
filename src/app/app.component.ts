944f4f574e8efebe8a159c8a938de37f33dbb750
rldcarvalho <rldcarvalhoo@gmail.com>
2023-06-19 19:09:02 -0300

    Style header-bg mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-adopet';
+  body: string = '';
+
+  constructor(private location: Location){}
+
+  ngOnInit(): void {
+    this.location.onUrlChange(x => {
+      if(x == '/') {
+        this.body = 'home';
+      }
+      if(x == '/signin' || x == '/signup') {
+        this.body = 'sign';
+      }
+      if(x == '/pets' || x == '/message' || x == '/profile') {
+        this.body = 'pets';
+      }
+    })
+  }
 }
