66c72e6bb1ccd38d7620bc5d46812e69086b4556
rldcarvalho <rldcarvalhoo@gmail.com>
2023-06-26 20:18:24 -0300

    Add message forms validator

--- src/app/components/pets/message/message.component.ts
+++ src/app/components/pets/message/message.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-message',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./message.component.scss']
 })
 export class MessageComponent {
+
+  messageForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {
+    this.messageForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(10)]],
+      phone: ['', [Validators.required, Validators.pattern(/^(?:(?:\+|00)?(55)\s?)?(?:(?:\(?[1-9][0-9]\)?)?\s?)?(?:((?:9\d|[2-9])\d{3})-?(\d{4}))$/)]],
+      petName: ['', [Validators.required]],
+      msg: ['', [Validators.required, Validators.minLength(30)]]
+    })
+  }
+
+  sendMessage(): void {}
 
 }
