6ed299f6905679416e69ba4ab55fc4ebba8871bb
rldcarvalho <rldcarvalhoo@gmail.com>
2023-07-05 21:03:13 -0300

    Add basic user register

--- src/app/components/home/signup/signup.component.ts
+++ src/app/components/home/signup/signup.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PasswordMatchValidator } from './PasswordMatch.validator';
+import { UserService } from 'src/app/shared/user/userService';
+import { User } from 'src/app/shared/user/user';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -15,7 +18,11 @@ export class SignupComponent {
   eyeIcon: string = "../../../assets/img/eye.svg";
   eyeClosedIcon: string = '../../../assets/img/eye-closed.svg';
 
-  constructor(private fb: FormBuilder){
+  constructor(
+    private fb: FormBuilder, 
+    private userService: UserService,
+    private router: Router
+    ){
     this.signupForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
       email: ['', [Validators.required, Validators.email]],
@@ -30,6 +37,24 @@ export class SignupComponent {
     this.hidePassword = !this.hidePassword;
   }
 
-  register(): void{}
+  register(): void{
+    if (this.signupForm.valid){
+      const newUser: User = {
+        email: this.signupForm.value.email,
+        password: this.signupForm.value.password,
+        name: this.signupForm.value.name,
+        phone: '',
+        city: '',
+        about: '',
+        image: ''
+      }
+      this.userService.createUser(newUser).subscribe(() => {
+        this.router.navigate(['/signin'])
+      }, (error) => {
+        console.error('Erro ao criar usuário:', error);
+      });
+    };
+
+  }
 
 }
